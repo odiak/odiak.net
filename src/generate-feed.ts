f62a1d460f9f9b6c4ea717bc145de9d270421ddd
Kaido Iwamoto <h6824.kaido@gmail.com>
2021-11-14 05:16:12 +0900

    Fix feed generation

--- src/generate-feed.ts
+++ src/generate-feed.ts
@@ -8,54 +8,57 @@ import unified from 'unified'
 import remarkHtml from 'remark-html'
 import { compareDateLike, getAllContents } from './contents'
 
-const feed = new Feed({
-  title: 'odiak.net',
-  id: 'https://odiak.net/',
-  link: 'https://odiak.net/',
-  language: 'ja',
-  description: '岩本海童の個人的なウェブサイトです。',
-  copyright: 'All rights reserved 2020, Kaido Iwamoto',
-  image: 'https://odiak.net/default-image.png',
-  favicon: 'https://odiak.net/favicon.png',
-  feedLinks: {
-    atom: '/atom-feed.xml'
-  },
-  author: {
-    name: 'Kaido Iwamoto',
-    email: 'kaido@odiak.net',
-    link: 'https://odiak.net/aboutme'
-  }
-})
-
-const contents = getAllContents()
-
-contents
-  .filter((c) => !c.isIntermediate)
-  .sort((a, b) => -compareDateLike(a.created, b.created))
-  .forEach((c) => {
-    const htmlContent = unified()
-      .use(remarkParse)
-      .use(wikiLinkPlugin, {
-        permalinks: contents
-          .filter((c) => c.isLinkedFromMultipleContents !== false)
-          .map((c) => c.slug),
-        pageResolver: (name: string) =>
-          contents.filter((c) => c.title.toLowerCase() === name.toLowerCase()).map((c) => c.slug),
-        hrefTemplate: (slug: string) => `/${slug}`
-      })
-      .use(remarkHtml)
-      .processSync(c.body).contents as string
-
-    feed.addItem({
-      title: c.title,
-      id: `https://odiak.net/${c.slug}`,
-      link: `https://odiak.net/${c.slug}`,
-      date: new Date(`${c.created.year}-${c.created.month}-${c.created.day} 00:00+9:00`),
-      content: htmlContent
-    })
+async function generateFeed() {
+  const feed = new Feed({
+    title: 'odiak.net',
+    id: 'https://odiak.net/',
+    link: 'https://odiak.net/',
+    language: 'ja',
+    description: '岩本海童の個人的なウェブサイトです。',
+    copyright: 'All rights reserved 2020, Kaido Iwamoto',
+    image: 'https://odiak.net/default-image.png',
+    favicon: 'https://odiak.net/favicon.png',
+    feedLinks: {
+      atom: '/atom-feed.xml'
+    },
+    author: {
+      name: 'Kaido Iwamoto',
+      email: 'kaido@odiak.net',
+      link: 'https://odiak.net/aboutme'
+    }
   })
-;(async () => {
+
+  const contents = await getAllContents()
+
+  contents
+    .filter((c) => !c.isIntermediate)
+    .sort((a, b) => -compareDateLike(a.created, b.created))
+    .forEach((c) => {
+      const htmlContent = unified()
+        .use(remarkParse)
+        .use(wikiLinkPlugin, {
+          permalinks: contents
+            .filter((c) => c.isLinkedFromMultipleContents !== false)
+            .map((c) => c.slug),
+          pageResolver: (name: string) =>
+            contents.filter((c) => c.title.toLowerCase() === name.toLowerCase()).map((c) => c.slug),
+          hrefTemplate: (slug: string) => `/${slug}`
+        })
+        .use(remarkHtml)
+        .processSync(c.body).contents as string
+
+      feed.addItem({
+        title: c.title,
+        id: `https://odiak.net/${c.slug}`,
+        link: `https://odiak.net/${c.slug}`,
+        date: new Date(`${c.created.year}-${c.created.month}-${c.created.day} 00:00+9:00`),
+        content: htmlContent
+      })
+    })
+
   const f = await fs.open('./public/atom-feed.xml', 'w')
   await f.write(feed.atom1())
   await f.close()
-})()
+}
+
+generateFeed()

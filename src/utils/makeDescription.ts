e15d48f637c17999dba2361f356e1fd3e5bf04eb
Kaido Iwamoto <kaido@odiak.net>
2022-02-24 10:34:47 +0900

    Strip wiki link

--- src/utils/makeDescription.ts
+++ src/utils/makeDescription.ts
@@ -5,6 +5,7 @@ import remarkGfm from 'remark-gfm'
 import remarkBreaks from 'remark-breaks'
 import strip from 'strip-markdown'
 import remarkStringify from 'remark-stringify'
+import { Node } from 'unist'
 
 export function makeDescription(content: string): string {
   return unified()
@@ -13,9 +14,24 @@ export function makeDescription(content: string): string {
     .use(remarkBreaks)
     .use(wikiLinkPlugin, { permalinks: [] })
     .use(remarkStringify)
+    .use(stripWikiLink)
     .use(strip)
     .processSync(content.split('\n\n', 1)[0])
     .toString()
     .trim()
-    .split('\n', 1)[0]
+    .replace(/\s+/g, ' ')
+}
+
+function stripWikiLink(): (node: Node) => Node {
+  function strip(node: Node): Node {
+    if (node.type === 'wikiLink') {
+      return { type: 'text', value: node.value }
+    }
+    if (!Array.isArray(node.children)) {
+      return node
+    }
+    return { ...node, children: (node.children as Node[]).map(strip) }
+  }
+
+  return strip
 }
